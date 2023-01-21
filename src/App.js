40fa42c5fcd08be6006f4d1a8f8a0c2a8cdb0ed5
Hiral1298 <121249450+Hiral1298@users.noreply.github.com>
2023-01-21 19:10:43 +0000

    isuue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,350 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  let json = [
+    // {
+    //   "id": 149,
+    //   "full_name": "Shilpa Dafda",
+    //   "email": "shilpa.d@samcomtechnobrains.com",
+    //   "vendor": {
+    //     "Maharastra": {
+    //       "Mumbai": [
+    //         {
+    //           "vendor_id": 34,
+    //           "vendor__user__company__name": "Ceramic Studio",
+    //           "vendor__user__company__state__name": "Maharastra",
+    //           "vendor__user__company__state__id": 2,
+    //           "vendor__user__company__city__id": 2,
+    //           "vendor__user__company__city__name": "Mumbai",
+    //           "targets": [],
+    //           "totalQuantity": 0
+    //         }
+    //       ]
+    //     },
+    //     "Punjab": {
+    //       "Ludhiana": [
+    //         {
+    //           "vendor_id": 35,
+    //           "vendor__user__company__name": "Ved Steel Impex",
+    //           "vendor__user__company__state__name": "Punjab",
+    //           "vendor__user__company__state__id": 6,
+    //           "vendor__user__company__city__id": 9,
+    //           "vendor__user__company__city__name": "Ludhiana",
+    //           "targets": [],
+    //           "totalQuantity": 0
+    //         }
+    //       ]
+    //     }
+    //   }
+    // },
+    // {
+    //   "id": 148,
+    //   "full_name": "Dhaval Varmora",
+    //   "email": "manishpatel199820@gmail.com",
+    //   "vendor": {}
+    // },
+    {
+      "id": 118,
+      "full_name": "Manish Varmora",
+      "email": "manish@samcomtechnobrains.com",
+      "vendor": {
+        "Gujarat": {
+          "Ahmedabad": [
+            {
+              "vendor_id": 29,
+              "vendor__user__company__name": "KRUPEX INDIA",
+              "vendor__user__company__state__name": "Gujarat",
+              "vendor__user__company__state__id": 1,
+              "vendor__user__company__city__id": 1,
+              "vendor__user__company__city__name": "Ahmedabad",
+              "targets": [
+                {
+                  "brand_id": 63,
+                  "qty": 5916,
+                  "achived": 137
+                },
+                {
+                  "brand_id": 64,
+                  "qty": 5798,
+                  "achived": 12
+                },
+                {
+                  "brand_id": 65,
+                  "qty": 6348,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 66,
+                  "qty": 5562,
+                  "achived": 150
+                },
+                {
+                  "brand_id": 67,
+                  "qty": 1,
+                  "achived": 0
+                }
+              ],
+              "totalQuantity": 0
+            },
+            {
+              "vendor_id": 30,
+              "vendor__user__company__name": "Dev Plastic",
+              "vendor__user__company__state__name": "Gujarat",
+              "vendor__user__company__state__id": 1,
+              "vendor__user__company__city__id": 1,
+              "vendor__user__company__city__name": "Ahmedabad",
+              "targets": [
+                {
+                  "brand_id": 63,
+                  "qty": 1000,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 64,
+                  "qty": 1102,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 65,
+                  "qty": 862,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 66,
+                  "qty": 1339,
+                  "achived": 83
+                }
+              ],
+              "totalQuantity": 0
+            },
+            {
+              "vendor_id": 32,
+              "vendor__user__company__name": "Vidhi Ceramic & Sanitary",
+              "vendor__user__company__state__name": "Gujarat",
+              "vendor__user__company__state__id": 1,
+              "vendor__user__company__city__id": 1,
+              "vendor__user__company__city__name": "Ahmedabad",
+              "targets": [
+                {
+                  "brand_id": 63,
+                  "qty": 1301,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 64,
+                  "qty": 1170,
+                  "achived": 12
+                },
+                {
+                  "brand_id": 65,
+                  "qty": 1140,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 66,
+                  "qty": 770,
+                  "achived": 0
+                }
+              ],
+              "totalQuantity": 0
+            }
+          ],
+          "Vadodara": [
+            {
+              "vendor_id": 31,
+              "vendor__user__company__name": "Punit Infosys",
+              "vendor__user__company__state__name": "Gujarat",
+              "vendor__user__company__state__id": 1,
+              "vendor__user__company__city__id": 6,
+              "vendor__user__company__city__name": "Vadodara",
+              "targets": [
+                {
+                  "brand_id": 63,
+                  "qty": 1145,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 64,
+                  "qty": 1346,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 65,
+                  "qty": 1850,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 66,
+                  "qty": 1170,
+                  "achived": 0
+                }
+              ],
+              "totalQuantity": 0
+            }
+          ],
+          "Rajkot": [
+            {
+              "vendor_id": 33,
+              "vendor__user__company__name": "Akash Plastics",
+              "vendor__user__company__state__name": "Gujarat",
+              "vendor__user__company__state__id": 1,
+              "vendor__user__company__city__id": 4,
+              "vendor__user__company__city__name": "Rajkot",
+              "targets": [
+                {
+                  "brand_id": 63,
+                  "qty": 830,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 64,
+                  "qty": 1010,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 65,
+                  "qty": 1450,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 66,
+                  "qty": 1530,
+                  "achived": 0
+                }
+              ],
+              "totalQuantity": 0
+            }
+          ]
+        },
+        "Punjab": {
+          "Ludhiana": [
+            {
+              "vendor_id": 35,
+              "vendor__user__company__name": "Ved Steel Impex",
+              "vendor__user__company__state__name": "Punjab",
+              "vendor__user__company__state__id": 6,
+              "vendor__user__company__city__id": 9,
+              "vendor__user__company__city__name": "Ludhiana",
+              "targets": [
+                {
+                  "brand_id": 63,
+                  "qty": 972,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 64,
+                  "qty": 674,
+                  "achived": 20
+                },
+                {
+                  "brand_id": 65,
+                  "qty": 1007,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 66,
+                  "qty": 589,
+                  "achived": 0
+                },
+                {
+                  "brand_id": 67,
+                  "qty": 300,
+                  "achived": 250
+                }
+              ],
+              "totalQuantity": 0
+            }
+          ]
+        }
+      }
+    }
+  ]
+  
+  function setData() {
+    let finalData= [];
+    let newobj = {};
+    
+    let data = json;
+    for (let i = 0; i < data.length; i++) {
+      let dataArray=[]
+      let vendorKeys=Object.keys(data[i].vendor);
+      let vendorValues=Object.values(data[i].vendor);
+      for (let j = 0; j < vendorKeys.length; j++) {
+        let cityVendors=[];
+        let vendorCityKeys=Object.keys(vendorValues[j])
+        let vendorCityValues=Object.values(vendorValues[j])
+        
+        for (let k = 0; k < vendorCityKeys.length; k++) {
+          let targetKeys=Object.keys(vendorCityValues[k])
+          let targetValues=Object.values(vendorCityValues[k])
+          // console.log(targetValues,"vendors")
+          // temp.push({id:vendors})
+          let temp=[];
+          for (let l = 0; l < targetKeys.length; l++) {
+            
+            console.log(targetValues[l],"vendors")
+            
+            let targetsData=targetValues[l].targets
+            for (let t = 0; t < targetsData.length; t++) {
+              temp.push({brand_id:targetsData[t].brand_id,achived:0})
+              for (let d = 0; d < temp.length; d++) {
+                console.log(temp[d].achived,"achived00",temp[d].brand_id===targetsData[t].brand_id,temp[d].brand_id,"====",targetsData[t].brand_id,"targetsData[t].brand_id---------")
+              if(temp[d].brand_id===targetsData[t].brand_id){
+                temp[d].achived=temp[d].achived+targetsData[t].achived
+              }
+              
+              }
+          }
+      }
+      
+    //  let finalData = await sumData(temp)
+    //  console.log(finalData,"finalData finalData",temp.length)
+    let tempData=temp.reduce((acc, item) => {
+      let existItem = acc.find(({brand_id}) => item.brand_id === brand_id);
+     console.log(existItem,"existItem")
+      if(existItem) {
+        console.log(existItem,item.achived,"item achived ********************")
+        existItem.achived += item.achived;
+      } else {
+        acc.push(item);
+      }
+      console.log(acc,"accaccaccacc")
+      return acc;
+    },[]);
+      cityVendors.push({id:vendorCityKeys[k],vendors:vendorCityValues[k],total:tempData})
+      }
+        dataArray.push({id:vendorKeys[j],city:cityVendors,total:["will work"]})
+      }
+      data[i].dataArray=dataArray
+      finalData.push(data[i])
+    }
+
+    return console.log(finalData,"FINALLL")
+  }
+
+  // useEffect(()=>{
+  // },[])
+  
+  
+  function sumData(arrayData){
+    console.log(arrayData.length,'START0000000000000000000000')
+   return arrayData.reduce((acc, item) => {
+      let existItem = acc.find(({brand_id}) => item.brand_id === brand_id);
+     console.log(existItem,"existItem")
+      if(existItem) {
+        console.log(item.achived,"item achived ")
+        existItem.achived += item.achived;
+      } else {
+        acc.push(item);
+      }
+      console.log(acc,"accaccaccacc")
+      return acc;
+    },[]);
+  }
+  console.log(setData(),"Function call");
+
   return (
     <div className="App">
       <header className="App-header">
